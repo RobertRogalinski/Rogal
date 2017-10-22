e5190ad05c46a2cbb778ad243a68a00a296df454
Robert Rogaliński <rrogalinski@linux-p110-15.inf.ug.edu.pl>
2017-10-22 13:37:38 +0200

    Zadanie 6

---
+++ Dokumenty/22.10.2017/zad6/zad6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int  main(int argc, char const *argv[]) {
+int n1=5+3*8/2-3;
+printf("n1:%d\n", n1);
+int n2=2%2+2*2-2/2;
+printf("n1:%d\n", n2 );
+int n3=2*4*(5+9/2);
+printf("n3:%d\n", n3 );
+
+  return 0;
+}
