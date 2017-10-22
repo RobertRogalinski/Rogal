88c80036dfbb8d221806a0060d63dddceeb1b2ab
Robert Rogaliński <rrogalinski@linux-p110-15.inf.ug.edu.pl>
2017-10-22 12:40:57 +0200

    Zadanie 3

---
+++ Dokumenty/22.10.2017/zad3/Algebra.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[]) {
+  /* Wszystkie dotepne wyrażenia arytmetyczne */
+  printf("+; ""%d\n", 2+2);
+  printf("-: %d\n", 3-2);
+  printf("*: ""%d\n", 3*2);
+printf("/: ""%d\n", 6/2 );
+printf("|: ""%d\n", 10|5 );
+printf("&: ""%d\n", 20&5 );
+printf("||: ""%d\n", 24||6 );
+printf("&&: ""%d\n",26&&4 );
+printf(">=: ""%d\n", 30 >=5 );
+printf("<: ""%d\n", 45<5);
+printf(">: ""%d\n",  415>5);
+printf("==: ""%d\n", 48==45 );
+printf("<=: ""%d\n", 45<=45 );
+printf(">>: ""%d\n", 45>>8 );
+printf("<<: %d\n",  58<<20);
+  return 0;
+}
